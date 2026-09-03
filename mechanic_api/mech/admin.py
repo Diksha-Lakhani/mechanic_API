11aa0bed8a06c71017a0a28dba58c274a08b5804
diksha <dee.mensionless@gmail.com>
2026-09-03 14:32:48 +0530

    add admin panel for diaplaying, filtering and searching  data

--- mechanic_api/mech/admin.py
+++ mechanic_api/mech/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Mechanic, ServiceRequest
 
-# Register your models here.
+#says how to display search and filter data in admin panel
+@admin.register(Mechanic)
+class MechanicAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'phone', 'location', 'rating', 'is_open']
+    search_fields = ['name', 'location']
+
+@admin.register(ServiceRequest)
+class ServiceRequestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer_name', 'mechanic', 'service', 'status', 'created_at']
+    list_filter = ['status']
+    search_fields = ['customer_name', 'vehicle_number']
